1bd33c8cc47cc16d5b552e92ada4bd1714a7225d
Oriol RC <quantic27@gmail.com>
2024-10-21 12:20:12 +0200

    Imrpove security

--- index.php
+++ index.php
@@ -5,18 +5,26 @@ require_once 'vendor/autoload.php';
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
+// Secure session management to avoid session fixation.
+session_start();
+session_regenerate_id(true);
+
 $loader = new FilesystemLoader('templates');
 $twig   = new Environment($loader);
 
-$page   = filter_var($_GET['page'] ?? 'index', FILTER_SANITIZE_STRING);
+// Sanitize and validate input var.
+$page   = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+$page   = $page ?: 'index';
 
 try {
     $file = 'pages/' . $page . '.html.twig';
     if (file_exists('templates/' . $file)) {
         print $twig->render($file);
     } else {
+        error_log('Template file not found: ' . $file);
         print $twig->render('front.html.twig');
     }
 } catch (Exception $e) {
-    print 'Error de renderitzat:' . $e->getMessage();
+    error_log('Rendering error: ' . $e->getMessage());
+    print 'An error occurred. Please try again later.';
 }
